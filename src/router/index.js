dbf7a826d011378d33c7ca58878b8e8f66b2cbd9
Jacob Wang <jiahao.king@outlook.com>
2021-12-11 15:44:58 +0800

    路由组件传参

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@
  * @Author: inzh
  * @Date: 2021-12-10 21:23:38
  * @LastEditors: inzh
- * @LastEditTime: 2021-12-10 22:31:20
+ * @LastEditTime: 2021-12-11 15:41:52
  * @Description:
  */
 import Vue from 'vue'
@@ -45,14 +45,32 @@ const routes = [
   },
   {
     name: 'search',
-    //  path: '/search/:searchKeyWord', 只匹配  /search/xxx
     //  path: '/search', 会匹配诸如 /search，/search?searchKeyWord=xxxx 的路径
     path: '/search',
     component: Search,
     meta: {
       showFooter: true
-    }
+    },
+    // URL /search?searchKeyWord=123 会将 {query: '123'} 作为属性，传递给 Search 组件
+    // 组件中通过 props: ['query'] 获得该属性
+    props: route => ({ query: route.query.searchKeyWord })
+
+    // 假设只需要给组件传静态属性， 此时 searchKeyWord 是静态的，值为 123
+    // props: { searchKeyWord: '123' }
   },
+  {
+    name: 'search',
+    //  path: '/search/:searchKeyWord', 只匹配  /search/xxx
+    path: '/search/:searchKeyWord',
+    component: Search,
+    meta: {
+      showFooter: true
+    },
+    // 如果 props 被设置为 true，则 route.params 将会被设置为组件属性
+    // 组件中通过 props: ['searchKeyWord'] 获得该属性
+    props: true
+  },
+
 ]
 
 const router = new VueRouter({
--- src/views/Search/index.vue
+++ src/views/Search/index.vue
@@ -2,16 +2,23 @@
  * @Author: inzh
  * @Date: 2021-12-10 20:57:04
  * @LastEditors: inzh
- * @LastEditTime: 2021-12-10 20:57:59
+ * @LastEditTime: 2021-12-11 15:40:47
  * @Description:
 -->
 <template>
-  <h1>我是Search组件</h1>
+  <div>
+    <!-- 通过$route获取query值，耦合度太高 -->
+    <!-- <h1>{{ $route.query.searchKeyWord }}</h1> -->
+
+    <h1>{{ query }}</h1>
+  </div>
 </template>
 
 <script>
 export default {
-
+  // 在路由中设置 props: true 则 route.params 会被设置为组件属性
+  // 在路由中设置 props: route=>({query: route.query.searchKeyWord})
+  props: ['searchKeyWord', 'query']
 }
 </script>
 
