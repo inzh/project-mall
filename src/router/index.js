ffa3d03b58d246968e5bdcc969bf081542e486be
Jacob Wang <jiahao.king@outlook.com>
2021-12-18 11:42:53 +0800

    产品详情页静态组件+路由跳转+滚动行为

--- src/router/index.js
+++ src/router/index.js
@@ -2,94 +2,23 @@
  * @Author: inzh
  * @Date: 2021-12-10 21:23:38
  * @LastEditors: inzh
- * @LastEditTime: 2021-12-16 17:01:27
+ * @LastEditTime: 2021-12-18 11:41:26
  * @Description:
  */
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '@/views/Home'
-import Login from '@/views/Login'
-import Register from '@/views/Register'
-import Search from '@/views/Search'
-
+import routes from './routes'
 Vue.use(VueRouter)
 
-const routes = [
-  {
-    path: '',
-    redirect: '/home'
-  },
-  {
-    name: 'home',
-    path: '/home',
-    component: Home,
-    meta: {
-      showFooter: true
-    }
-  },
-  {
-    name: 'login',
-    path: '/login',
-    component: Login,
-    meta: {
-      showFooter: false
-    }
-  },
-  {
-    name: 'register',
-    path: '/register',
-    component: Register,
-    meta: {
-      showFooter: false
-    }
-  },
-  // {
-  //   name: 'search',
-  //   //  path: '/search', 会匹配诸如 /search，/search?searchKeyWord=xxxx 的路径
-  //   path: '/search',
-  //   component: Search,
-  //   meta: {
-  //     showFooter: true
-  //   },
-  //   // URL /search?searchKeyWord=123 会将 {query: '123'} 作为属性，传递给 Search 组件
-  //   // 组件中通过 props: ['query'] 获得该属性
-  //   props: route => ({ query: route.query.searchKeyWord })
 
-  //   // 假设只需要给组件传静态属性， 此时 searchKeyWord 是静态的，值为 123
-  //   // props: { searchKeyWord: '123' }
-  // },
-  // {
-  //   name: 'search',
-  //   //  path: '/search/:searchKeyWord', 只匹配  /search/xxx
-  //   path: '/search/:searchKeyWord',
-  //   component: Search,
-  //   meta: {
-  //     showFooter: true
-  //   },
-  //   // 如果 props 被设置为 true，则 route.params 将会被设置为组件属性
-  //   // 组件中通过 props: ['searchKeyWord'] 获得该属性
-  //   props: true
-  // },
-  {
-    name: 'search',
-    // path: '/search/:searchKeyWord?', 既可以匹配 /search/searchKeyWord 也可以 /search?searchKeyWord
-    // 还可以 /search/searchKeyWord？searchKeyWord=xxx
-    path: '/search/:keyword?',
-    component: Search,
-    meta: {
-      showFooter: true
-    },
-    // 如果 props 被设置为 true，则 route.params 将会被设置为组件属性
-    // 组件中通过 props: ['searchKeyWord'] 获得该属性
-    props: true
-  },
-
-]
 
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes
+  routes,
+  scrollBehavior (to, from, savedPosition) {
+    return { y: 0 }
+  }
 })
 
 
---
+++ src/router/routes.js
@@ -0,0 +1,94 @@
+/*
+ * @Author: inzh
+ * @Date: 2021-12-18 11:38:50
+ * @LastEditors: inzh
+ * @LastEditTime: 2021-12-18 11:39:57
+ * @Description:
+ */
+
+import Home from '@/views/Home'
+import Login from '@/views/Login'
+import Register from '@/views/Register'
+import Search from '@/views/Search'
+import Detail from '@/views/Detail'
+const routes = [
+  {
+    path: '',
+    redirect: '/home'
+  },
+  {
+    name: 'home',
+    path: '/home',
+    component: Home,
+    meta: {
+      showFooter: true
+    }
+  },
+  {
+    name: 'login',
+    path: '/login',
+    component: Login,
+    meta: {
+      showFooter: false
+    }
+  },
+  {
+    name: 'register',
+    path: '/register',
+    component: Register,
+    meta: {
+      showFooter: false
+    }
+  },
+  // {
+  //   name: 'search',
+  //   //  path: '/search', 会匹配诸如 /search，/search?searchKeyWord=xxxx 的路径
+  //   path: '/search',
+  //   component: Search,
+  //   meta: {
+  //     showFooter: true
+  //   },
+  //   // URL /search?searchKeyWord=123 会将 {query: '123'} 作为属性，传递给 Search 组件
+  //   // 组件中通过 props: ['query'] 获得该属性
+  //   props: route => ({ query: route.query.searchKeyWord })
+
+  //   // 假设只需要给组件传静态属性， 此时 searchKeyWord 是静态的，值为 123
+  //   // props: { searchKeyWord: '123' }
+  // },
+  // {
+  //   name: 'search',
+  //   //  path: '/search/:searchKeyWord', 只匹配  /search/xxx
+  //   path: '/search/:searchKeyWord',
+  //   component: Search,
+  //   meta: {
+  //     showFooter: true
+  //   },
+  //   // 如果 props 被设置为 true，则 route.params 将会被设置为组件属性
+  //   // 组件中通过 props: ['searchKeyWord'] 获得该属性
+  //   props: true
+  // },
+  {
+    name: 'search',
+    // path: '/search/:searchKeyWord?', 既可以匹配 /search/searchKeyWord 也可以 /search?searchKeyWord
+    // 还可以 /search/searchKeyWord？searchKeyWord=xxx
+    path: '/search/:keyword?',
+    component: Search,
+    meta: {
+      showFooter: true
+    },
+    // 如果 props 被设置为 true，则 route.params 将会被设置为组件属性
+    // 组件中通过 props: ['searchKeyWord'] 获得该属性
+    props: true
+  },
+  {
+    name: 'detail',
+    path: '/detail/:skuId',
+    component: Detail,
+    meta: {
+      showFooter: true
+    }
+  }
+
+]
+
+export default routes
